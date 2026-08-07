a5de256cbb35bff91a548ecd13c3762fd55428e9
Aayush Vyas <aayush25vyas@gmail.com>
2026-08-07 20:00:22 +0530

    Map Component completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-import React from 'react'
-import "./App.css"
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
-import Map from './components/Map/Map'
+import React from "react";
+import "./App.css";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Map from "./components/Map/Map";
 
 function App() {
   return (
     <div>
-      <Header/>
-      <Map />
+      <Header />
+      <Map
+        center={[51.505, -0.09]}
+        zoom={13}
+        style={{ height: "800px", width: "inherit" }}
+      />
       <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,8 +1,8 @@
-import { MapContainer, Marker, TileLayer } from "react-leaflet";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import envVariables from "../../constants/envVariables";
 
-function Map({ center, zoom, style, markerPosition }) {
+function Map({ center, zoom, style }) {
   const { mapUrl } = envVariables;
   return (
     <div className="map">
@@ -11,7 +11,11 @@ function Map({ center, zoom, style, markerPosition }) {
           url={mapUrl}
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         />
-        <Marker position={markerPosition} />
+        <Marker position={center}>
+          <Popup>
+            Here Wild fire <br /> occurs
+          </Popup>
+        </Marker>
       </MapContainer>
     </div>
   );
