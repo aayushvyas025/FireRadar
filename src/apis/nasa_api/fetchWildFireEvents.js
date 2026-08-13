4b2686e264ad8a623c1e10be16eb279b67a7b657
Aayush Vyas <aayush25vyas@gmail.com>
2026-08-13 22:59:22 +0530

    adding the cache feature to our events api

--- src/apis/nasa_api/fetchWildFireEvents.js
+++ src/apis/nasa_api/fetchWildFireEvents.js
@@ -1,4 +1,29 @@
+const CACHE_KEY = "fireRadar_wildfire_events";
+const CACHE_TTL = 15 * 60 * 1000;
+
 async function fetchWildFireEvents() {
+  const cacheData = localStorage.getItem(CACHE_KEY);
+
+  if (cacheData) {
+    // parsing cache json
+    const cache = JSON.parse(cacheData);
+    // create cache
+    const isCacheValid = Date.now() - cache.timestamp < CACHE_TTL;
+
+    if (isCacheValid) {
+      console.log("Using cached wildfire data");
+
+      return {
+        success: true,
+        message: "Wildfire events loaded from cache",
+        data: cache.data,
+      };
+    }
+  }
+
+  console.log("Cache expired");
+
+  console.log("Fetching wildfire events from API");
   try {
     const response = await fetch(
       `https://eonet.gsfc.nasa.gov/api/v2.1/events`,
@@ -8,12 +33,21 @@ async function fetchWildFireEvents() {
       throw new Error("Failed to fetch wildfire events");
     }
     const data = await response.json();
+
+    const wildfireEvents = data?.events.filter((event) =>
+      event?.title.toLowerCase().includes("wildfire"),
+    );
+
+    // set the items in filtered data + timestamp
+    localStorage.setItem(
+      CACHE_KEY,
+      JSON.stringify({ data: wildfireEvents, timestamp: Date.now() }),
+    );
+
     return {
       success: true,
       message: "Wildfire events fetched successfully",
-      data: data?.events.filter((event) =>
-        event?.title.toLowerCase().includes("wildfire"),
-      ),
+      data: wildfireEvents,
     };
   } catch (error) {
     console.error(`Error While fetching wildfire events api ${error.message}`);
